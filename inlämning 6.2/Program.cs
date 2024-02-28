c5b4d41322c6081617d9f3725dac10e6ad1bb24c
vhvalbone <albin06svensson@gmail.com>
2024-02-28 10:32:52 +0100

    Add project files.

---
+++ inlämning 6.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int störstaTalet = Störst(10, 5);
+        Console.WriteLine("Det största talet är: " + störstaTalet);
+    }
+
+    static int Störst(int tal1, int tal2)
+    {
+        if (tal1 > tal2)
+        {
+            return tal1;
+        }
+        else if (tal2 > tal1)
+        {
+            return tal2;
+        }
+        else
+        {
+            return tal1; 
+        }
+    }
+}
+
